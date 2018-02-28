66896bb0589b39855720c3c474a12f5c4ca20028
Luke Puplett <lukepuplett@hotmail.com>
2018-02-28 12:30:48 +0000

    * Added EmptyLoggerFactory

---
+++ src/Instrumentation/Instrumentation/EmptyLoggerFactory.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.Extensions.Logging;
+
+namespace Evoq.Instrumentation
+{
+    /// <summary>
+    /// Creates instances of the <see cref="EmptyLogger"/> class.
+    /// </summary>
+    public sealed class EmptyLoggerFactory : ILoggerFactory
+    {
+        /// <summary>
+        /// Does nothing on this empty factory.
+        /// </summary>
+        /// <param name="provider">Not used.</param>
+        public void AddProvider(ILoggerProvider provider) { }
+
+        /// <summary>
+        /// Creates an instance of an <see cref="EmptyLogger"/>.
+        /// </summary>
+        /// <param name="categoryName">The name to give to the logger.</param>
+        /// <returns>An logger that does nothing.</returns>
+        public ILogger CreateLogger(string categoryName)
+        {
+            return new EmptyLogger(categoryName);
+        }
+
+        /// <summary>
+        /// Does nothing on this empty logger.
+        /// </summary>
+        public void Dispose() { }
+    }
+}
