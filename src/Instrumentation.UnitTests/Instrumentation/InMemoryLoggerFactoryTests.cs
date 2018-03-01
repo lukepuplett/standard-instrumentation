1ae1eeef7dfbdad247129d03850b6f323d01a3ac
Luke Puplett <lukepuplett@hotmail.com>
2018-03-01 15:01:53 +0000

    Added InMemoryLoggerFactory and tests.

---
+++ src/Instrumentation.UnitTests/Instrumentation/InMemoryLoggerFactoryTests.cs
@@ -0,0 +1,54 @@
+﻿
+using Microsoft.Extensions.Logging;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Evoq.Instrumentation.Instrumentation
+{
+    [TestClass]
+    public class InMemoryLoggerFactoryTests
+    {
+        [TestMethod]
+        public void InMemoryLoggerFactory__ctor__when_called__then__returns()
+        {
+            InMemoryLoggerFactory loggerFactory = new InMemoryLoggerFactory();
+
+            Assert.IsNotNull(loggerFactory);
+        }
+
+        [TestMethod]
+        public void InMemoryLoggerFactory__CreateLogger__when_called_with_a_name__then__logger_has_name()
+        {
+            InMemoryLoggerFactory loggerFactory = new InMemoryLoggerFactory();
+
+            var logger = loggerFactory.CreateLogger("Test") as InMemoryLogger;
+            
+            Assert.AreEqual("Test", logger.Name);            
+        }
+
+        [TestMethod]
+        public void InMemoryLoggerFactory__CreateLogger__when_logger_used_once__then__factory_has_single_message()
+        {
+            InMemoryLoggerFactory loggerFactory = new InMemoryLoggerFactory();
+
+            var logger = loggerFactory.CreateLogger("Test") as InMemoryLogger;
+
+            logger.LogDebug("Hey");
+
+            Assert.AreEqual(1, loggerFactory.Messages.Count);
+        }
+
+        [TestMethod]
+        public void InMemoryLoggerFactory__CreateLogger__when_two_loggers_created_and_used_once_each__then__factory_has_both_messages()
+        {
+            InMemoryLoggerFactory loggerFactory = new InMemoryLoggerFactory();
+
+            var logger = loggerFactory.CreateLogger("Test") as InMemoryLogger;
+            var secondLogger = loggerFactory.CreateLogger("Second");
+
+            logger.LogDebug("Hey");
+            secondLogger.LogDebug("Hey again");
+
+            Assert.AreEqual(2, loggerFactory.Messages.Count);
+        }
+    }
+}
---
+++ src/Instrumentation/Instrumentation/InMemoryLoggerFactory.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.Extensions.Logging;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+
+namespace Evoq.Instrumentation
+{
+    /// <summary>
+    /// Creates instances of the <see cref="EmptyLogger"/> class.
+    /// </summary>
+    public sealed class InMemoryLoggerFactory : ILoggerFactory
+    {
+        private ConcurrentBag<string> _messageStore = new ConcurrentBag<string>();
+
+        /// <summary>
+        /// Gets a view of the messages logged so far.
+        /// </summary>
+        public IReadOnlyCollection<string> Messages => _messageStore;
+
+        /// <summary>
+        /// Does nothing on this empty factory.
+        /// </summary>
+        /// <param name="provider">Not used.</param>
+        public void AddProvider(ILoggerProvider provider) { }
+
+        /// <summary>
+        /// Creates an instance of an <see cref="EmptyLogger"/>.
+        /// </summary>
+        /// <param name="categoryName">The name to give to the logger.</param>
+        /// <returns>An logger that does nothing.</returns>
+        public ILogger CreateLogger(string categoryName)
+        {
+            return new InMemoryLogger(categoryName, _messageStore);
+        }
+
+        /// <summary>
+        /// Does nothing on this empty logger.
+        /// </summary>
+        public void Dispose() { }
+    }
+}
